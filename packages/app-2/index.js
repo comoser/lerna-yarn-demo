01b167b84750b6b74c14efb19d2d58ee71b471ca
David Alecrim <d.alecrim@xgeeks.io>
2021-02-01 16:28:04 +0000

    feat(app-2): new feature

--- packages/app-2/index.js
+++ packages/app-2/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json('hi there from app-2 and here is a change to detect!');
+    res.json('hi there from app-2 and here is a change to detect! next version');
 });
 
 app.get('/hi', (req, res) => {
