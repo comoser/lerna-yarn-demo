a087efcce306797b1a7ae2f6100afdb57e4fd2f9
David Alecrim <d.alecrim@xgeeks.io>
2021-02-02 11:55:02 +0000

    chore(release): update release script

--- release.js
+++ release.js
@@ -5,7 +5,7 @@ const parts = process.env.TAG_NAME && process.env.TAG_NAME.split('@');
 console.log('App to deploy: ', parts && parts[0]);
 console.log('Version to deploy: ', parts && parts[1]);
 
-console.log('changelog to update in docs: ', process.cwd() + '/' + parts[0] + 'CHANGELOG.md');
+console.log('changelog to update in docs: ', process.cwd() + '/' + parts[0] + '/' + 'CHANGELOG.md');
 
 console.log('run script: ', 'yarn run:build:' + parts[0]);
 
