ba365f6d5551f75ba6943439700f50157f4e578f
David Alecrim <d.alecrim@xgeeks.io>
2021-02-03 11:06:57 +0000

    feat(feat-b): for app-1

--- packages/app-1/index.js
+++ packages/app-1/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json('hi there from app-1. New feature for app-1');
+    res.json('hi there from app-1. New feature for app-1. Yey.');
 });
 
 app.listen(3000, () => {
