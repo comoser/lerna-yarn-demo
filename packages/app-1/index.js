b360a91a2b2d702b440e454144b4708fe6799927
David Alecrim <d.alecrim@xgeeks.io>
2021-03-26 11:09:04 +0000

    chore(spacing): remove extra spacing

--- packages/app-1/index.js
+++ packages/app-1/index.js
@@ -5,7 +5,6 @@ const app = express();
 app.get('/', (req, res) => {
     res.json('hi there from app-1. Feat 1. plus on11');
 });
-
 app.listen(3000, () => {
     console.log('app listening on port 3000');
 });
